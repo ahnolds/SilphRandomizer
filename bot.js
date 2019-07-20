dae73d79cdd13e36197e3dbb7aafd47dd27c2c99
Alec Cooper <ahnolds@gmail.com>
2019-07-19 22:41:53 -0400

    Whoops :( don't commit keys

--- bot.js
+++ bot.js
@@ -1,26 +1,26 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const {prefix, seasons} = require('./config.json');
+const {token} = require('./auth.json');
 const client = new Discord.Client();
  
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
  
-client.on('message', msg => {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (msg.content.substring(0, 1) == config.prefix) {
-        var args = msg.content.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !mirror
-            case 'mirror':
-				msg.channel.send('asdf');
-            break;
-         }
-     }
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	
+	const args = message.content.slice(prefix.length).split(' ');
+	const command = args.shift().toLowerCase();
+
+	switch(command) {
+		case 'mirror':
+			if (!args.length && Object.keys(seasons).length != 1) {
+			return message.channel.send(`Pick a season ${message.author} ${Object.keys(seasons).length}!`);
+			}
+			message.channel.send('asdf');
+		break;
+	 }
 });
  
-client.login(config.token);
+client.login(token);
